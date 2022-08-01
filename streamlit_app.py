27b52d2676ae81de68b7b8d0b15b82d66a1009a0
AnshitaSinha <49789597+AnshitaSinha@users.noreply.github.com>
2022-08-01 17:17:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit
 streamlit.title('Hello Have a Good Day')
 streamlit.header('Gratitude')
 streamlit.text('Reading is good')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
